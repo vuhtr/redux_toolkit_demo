c5807b11a4f6c548b525ac83c3a7cffc628d2a14
H.T.Vu <trongvulqd@gmail.com>
2022-10-16 21:46:54 +0700

    [CLIENT] add navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+
+import { Container } from "@mui/material"
+
+import { useSelector, useDispatch } from "react-redux"
+import { logout } from "redux/userSlice"
+
+import pathNames from "routes/pathNames"
+
+export default function Navbar() {
+    const [showDropdown, setShowDropdown] = useState(false)
+
+    const user = useSelector((state) => state.user.userInfo)
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
+
+    return (
+        <div className="navbar">
+            <Container className="navbar__container">
+                <Link to="/" className="navbar__logo">
+                    Photo Gallery
+                </Link>
+
+                {user.email ? (
+                    <div className="navbar__user">
+                        <p className="navbar__user__name">{user.fullName}</p>
+                        <div
+                            className="navbar__user__avatar"
+                            onMouseEnter={() => setShowDropdown(true)}
+                            onMouseLeave={() => setShowDropdown(false)}
+                        >
+                            <img src={user.avatar} alt="user avatar" />
+                            {showDropdown && (
+                                <div className="navbar__user__dropdown">
+                                    <ul>
+                                        <li>Profile</li>
+                                        <li onClick={handleLogout}>Log out</li>
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                ) : (
+                    <ul className="navbar__link">
+                        <li>
+                            <Link to={pathNames.LOGIN}>Log In</Link>
+                        </li>
+                        <li>
+                            <Link to={pathNames.SIGNUP}>Sign Up</Link>
+                        </li>
+                    </ul>
+                )}
+
+                {/* <ul className="navbar__link">
+                        <li>
+                            <Link to={pathNames.LOGIN}>Log In</Link>
+                        </li>
+                        <li>
+                            <Link to={pathNames.SIGNUP}>Sign Up</Link>
+                        </li>
+                    </ul> */}
+
+                {/* User has logged out */}
+
+                {/* User are logging in */}
+                {/* <ul className="navbar__link">
+                <li><Link to="/">Profile</Link></li>
+            </ul> */}
+            </Container>
+        </div>
+    )
+}
