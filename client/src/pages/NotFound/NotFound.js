98e75b91840cbd860b5905480fecd8d64ec76421
H.T.Vu <trongvulqd@gmail.com>
2022-10-17 16:18:24 +0700

    [CLIENT] add NotFound page

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Container } from "@mui/material"
+import React from "react"
 
 export default function NotFound() {
-  return (
-    <div>NotFound</div>
-  )
+    return (
+        <div className="not-found">
+            <Container className="not-found__container">
+                <h1>Page not found...</h1>
+            </Container>
+        </div>
+    )
 }
